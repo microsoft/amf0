bc08e19c39c2e0a0e0b1552eb1de4ff184f1d1ee
Taylor Blau <me@ttaylorr.com>
2016-03-15 21:00:16 -0400

    number: refactor Number type

--- number_test.go
+++ number_test.go
@@ -2,52 +2,31 @@ package amf0
 
 import (
 	"bytes"
-	"encoding/binary"
-	"github.com/stretchr/testify/assert"
-	"math"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func simpleEncode(n float64) []byte {
-	un := math.Float64bits(n)
-	b := make([]byte, 8)
-	binary.BigEndian.PutUint64(b, un)
-	return append([]byte{MARKER_NUMBER}, b...)
-}
+func TestNumberEncoding(t *testing.T) {
+	s := Number(float64(0x4242))
 
-func TestNumberBuildsAndEncodes(t *testing.T) {
-	s := NewNumber()
-	s.SetNumber(0x4242)
-	assert.Equal(t, simpleEncode(0x4242), s.EncodeBytes())
+	buf := new(bytes.Buffer)
+	n, err := s.Encode(buf)
+
+	assert.Equal(t, 8, n)
+	assert.Nil(t, err)
+
+	assert.Equal(t, []byte{
+		64, 208, 144, 128, 0, 0, 0, 0,
+	}, buf.Bytes())
 }
 
 func TestNumberDecodes(t *testing.T) {
-	s := NewNumber()
-	s.SetNumber(0x4242)
-	data := s.EncodeBytes()[1:]
+	o := new(Number)
+	err := o.Decode(bytes.NewReader([]byte{
+		64, 208, 144, 128, 0, 0, 0, 0,
+	}))
 
-	o := NewNumber()
-	err := o.Decode(bytes.NewReader(data))
 	assert.Nil(t, err)
-	assert.Equal(t, float64(0x4242), o.GetNumber())
-}
-
-func BenchmarkNumberDecode(b *testing.B) {
-	in := NewNumber()
-	in.SetNumber(0x4242)
-	data := in.EncodeBytes()[1:]
-	out := NewNumber()
-
-	for i := 0; i < b.N; i++ {
-		out.Decode(bytes.NewReader(data))
-	}
-}
-
-func BenchmarkNumberEncode(b *testing.B) {
-	in := NewNumber()
-	in.SetNumber(0x4242)
-
-	for i := 0; i < b.N; i++ {
-		in.EncodeBytes()
-	}
+	assert.Equal(t, float64(0x4242), float64(*o))
 }
