016d0a1b8db04c44c7aa524882949c54c203da5a
Taylor Blau <me@ttaylorr.com>
2016-04-03 13:38:15 -0400

    amf0: introduce and implement the Bodyless interface

--- bodyless.go
+++ bodyless.go
@@ -8,6 +8,10 @@ import (
 type (
 	Null      struct{}
 	Undefined struct{}
+
+	Bodyless interface {
+		IsBodyless() bool
+	}
 )
 
 var (
@@ -19,8 +23,10 @@ func (n *Null) Decode(r io.Reader) error        { return nil }
 func (n *Null) Encode(w io.Writer) (int, error) { return 0, nil }
 func (n *Null) Marker() byte                    { return 0x05 }
 func (n *Null) Native() reflect.Type            { return reflect.TypeOf(n).Elem() }
+func (n *Null) IsBodyless() bool                { return true }
 
 func (u *Undefined) Decode(r io.Reader) error        { return nil }
 func (u *Undefined) Encode(w io.Writer) (int, error) { return 0, nil }
 func (u *Undefined) Marker() byte                    { return 0x06 }
 func (u *Undefined) Native() reflect.Type            { return reflect.TypeOf(u).Elem() }
+func (u *Undefined) IsBodyless() bool                { return true }
